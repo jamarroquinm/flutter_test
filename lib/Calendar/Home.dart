cc72943b172e8fe08b5e81fdea61ed16c84c88fa
Alberto <fignealberto@gmail.com>
2019-03-11 16:42:24 -0600

    Pruebas de formulario

--- lib/Calendar/Home.dart
+++ lib/Calendar/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'CalendarPainter.dart';
+import 'dart:math';
 
 class MyApp extends StatefulWidget {
   @override
@@ -7,6 +8,18 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String campo1;
+  final FocusNode _campo1Focus = FocusNode();
+  final FocusNode _campo2Focus = FocusNode();
+  final FocusNode _campo3Focus = FocusNode();
+  final _campo1Controller = TextEditingController();
+  final _campo2Controller = TextEditingController();
+  final _campo3Controller = TextEditingController();
+
+  @override
+  void initState(){
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +32,7 @@ class _MyAppState extends State<MyApp> {
       DateTime(2019, 3, 3) : Colors.lime,
     };
 
+    _campo1Focus.addListener(_validateCampo1Value);
 
     Offset _tapPosition;
 
@@ -33,36 +47,67 @@ class _MyAppState extends State<MyApp> {
 
     return MaterialApp(
       home: Scaffold(
+        //resizeToAvoidBottomInset: false,
         appBar: AppBar(title: Text("Calendario")),
         body: Padding(
           padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-/*
-              GestureDetector(
-                onTapUp: selectDate,
-                child: CustomPaint(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                CustomPaint(
                   child: Container(height: 200,),
-                  painter: CalendarPainter(month: month + 1, year: year),
+                  painter: CalendarPainter(
+                    month: month,
+                    year: year,
+                    selectionCallback: selectDate,
+                    highlightedDates: highlightedDates,
+                    backgroundColor: Colors.purple,
+                    daysNameFillColor: Colors.deepPurpleAccent,
+                    daysNameTextStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                    daysTextStyle: TextStyle(color: Colors.white),
+                  ),
                 ),
-              ),
+                Form(
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        //autofocus: true,
+                        focusNode: _campo1Focus,
+                        textInputAction: TextInputAction.next,
+                        validator: (String value) => (value == null || value == '') ? "falta campo 1" : null,
+                        keyboardType: TextInputType.text,
+/*
+                      onFieldSubmitted: (term){
+                        _onFieldSubmitted(context, _campo1Focus, _campo2Focus, _validateCampo1Value);
+                      },
 */
-              CustomPaint(
-                child: Container(height: 200,),
-                painter: CalendarPainter(
-                  month: month,
-                  year: year,
-                  selectionCallback: selectDate,
-                  highlightedDates: highlightedDates,
-                  backgroundColor: Colors.purple,
-                  daysNameFillColor: Colors.deepPurpleAccent,
-                  daysNameTextStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                  daysTextStyle: TextStyle(color: Colors.white),
+                        controller: _campo1Controller,
+                      ),
+                      TextFormField(
+                        focusNode: _campo2Focus,
+                        textInputAction: TextInputAction.next,
+                        validator: (String value) => (value == null || value == '') ? "falta campo 2" : null,
+                        keyboardType: TextInputType.text,
+                        controller: _campo2Controller,
+                      ),
+                      TextFormField(
+                        focusNode: _campo3Focus,
+                        textInputAction: TextInputAction.done,
+                        validator: (String value) => (value == null || value == '') ? "falta campo 1" : null,
+                        keyboardType: TextInputType.text,
+                        controller: _campo3Controller,
+                      ),
+                      RaisedButton(
+                        onPressed: _grabarForm,
+                        child: Text('Grabar'),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
 
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -72,5 +117,66 @@ class _MyAppState extends State<MyApp> {
     print('Home: día elegido=$selectedDay');
   }
 
+  void _grabarForm(){
+    print('_grabarForm()');
+  }
 
+  void _onEditingComplete(){
+    print('_onEditingComplete');
+  }
+
+  void _onFieldSubmitted(BuildContext context, FocusNode currentFocus,
+    FocusNode nextFocus, Function function){
+
+    if(currentFocus != null){
+      currentFocus.unfocus();
+    }
+
+    if(nextFocus != null){
+      FocusScope.of(context).requestFocus(nextFocus);
+    }
+
+    if(function != null){
+      function();
+    }
+ }
+
+  @override
+  void dispose() {
+    _campo1Focus.dispose();
+    super.dispose();
+  }
+  
+  void _validateCampo1Value(){
+    if(!_campo1Focus.hasFocus){
+      print('campo1 pierde el foco');
+      if(_campo1Controller.text == null || _campo1Controller.text == ''){
+        campo1 = null;
+        print('campo1 está vacío, no se valida');
+      } else if(campo1 != null &&
+          campo1 != '' &&
+          campo1 == _campo1Controller.text) {
+        print('campo1 ya fue validado');
+      } else {
+        print('inicia validación de campo1');
+        bool validacion = _randomValidation();
+        validacion = false;
+        if(validacion){
+          campo1 = _campo1Controller.text;
+          print('campo1 pasó la validación');
+        } else {
+          _campo2Focus.unfocus();
+          FocusScope.of(context).autofocus(_campo1Focus);
+          print('campo1 es inválido');
+        }
+      }
+    } else {
+      print('campo1 gana el foco');
+    }
+  }
+
+  bool _randomValidation(){
+    var rng = new Random();
+    return rng.nextBool();
+  }
 }
